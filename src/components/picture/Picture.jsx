c249372633c536c49c378e7c85cd1a0424d808cd
Slavik3011 <Slavik3011@yandex.ru>
2017-11-08 00:53:06 +0200

    add preloaders

--- src/components/picture/Picture.jsx
+++ src/components/picture/Picture.jsx
@@ -3,18 +3,53 @@ import { connect } from 'react-redux'
 
 import './picture.sass'
 
-export default ({ src, id, activePicture, setActivePicture }) => {
-    const setActive = () => {
-        setActivePicture(id);
-        console.log(id)
-    };
+export default class Picture extends React.Component {
+    constructor() {
+        super();
 
-    return (
-        <li className={ `picture ${id == activePicture ? 'active' : ''}`}>
-            <div>
-                <img src={src} onClick={setActive}/>
-                {/*{src}*/}
-            </div>
-        </li>
-    )
-};
+        this.setActive = this.setActive.bind(this);
+        this.load = this.load.bind(this);
+
+        this.state = {
+            load: false
+        }
+    }
+
+    setActive() {
+        let { setActivePicture, id } = this.props;
+        setActivePicture(id);
+    }
+
+    load() {
+        this.setState({
+            load: true
+        })
+    }
+
+
+    render() {
+        let { id, activePicture, src } = this.props;
+        return (
+            <li className={ `picture ${id == activePicture ? 'active' : ''}`}>
+                <div>
+                    <img className={this.state.load ? 'loaded': ''} src={src} onClick={this.setActive} onLoad={this.load} />
+                </div>
+                {this.state.load ? '' : <div className="preloader"></div>}
+            </li>
+        )
+    }
+}
+
+// export default ({ src, id, activePicture, setActivePicture }) => {
+//     const setActive = () => {
+//         setActivePicture(id);
+//     };
+//
+//     return (
+//         <li className={ `picture ${id == activePicture ? 'active' : ''}`}>
+//             <div>
+//                 <img src={src} onClick={setActive}/>
+//             </div>
+//         </li>
+//     )
+// };
